4ddd98822c95b4f1a6492fd95a5dcefb62867149
Saki Jane <sakicanbereached@gmail.com>
2016-02-24 13:26:24 -0800

    assignment 34 topics completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,16 @@ require "random_data"
 end
 topics = Topic.all
 
+# Create Sponsored Posts
+50.times do
+  SponsoredPost.create!(
+    topic: topics.sample,
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph,
+    price: 99
+  )
+end
+
 # Create Posts
 50.times do
   Post.create!(
@@ -29,5 +39,6 @@ end
 
 puts "Seeds finished"
 puts "#{Topic.count} topics created"
+puts "#{SponsoredPost.count} sponsored posts created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
