1f517fbc25fa7bc442341d22137766c645cbb5cc
Saki Jane <sakicanbereached@gmail.com>
2016-03-01 10:56:55 -0800

    Assignment 37: in progress

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     resources :posts, except: [:index]
   end
 
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :confirm]
 
   get 'about' => 'welcome#about'
 
