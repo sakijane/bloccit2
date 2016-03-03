67eb35cddc5d02b01dce54149a2ea8aa767f9016
Saki Jane <sakicanbereached@gmail.com>
2016-03-03 14:22:42 -0800

    checkpoint 39 completed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160226221938) do
+ActiveRecord::Schema.define(version: 20160303005259) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -37,9 +37,11 @@ ActiveRecord::Schema.define(version: 20160226221938) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "topic_id"
+    t.integer  "user_id"
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"
